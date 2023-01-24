8b9775822c6dc37969115243bb3e1bb2da61094b
Leandro Mac <lehnihon@hotmail.com>
2023-01-24 18:15:08 -0300

    INITAL COMMIT

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+var jsonParser = bodyParser.json();
+
+app.post("/pdf-to-image", jsonParser, async (req, res) => {
+  const { fromBase64 } = require("pdf2pic");
+  const { pdf } = req.body;
+
+  if (!pdf) {
+    return res
+      .status(400)
+      .json({ message: "Envie o input body pdf em base64" });
+  }
+
+  const options = {
+    density: 100,
+    format: "jpg",
+    width: 1200,
+    height: 1800,
+  };
+
+  const convert = fromBase64(pdf, options);
+  const image = await convert(1, true);
+
+  return res.status(200).json({ image: image.base64 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
